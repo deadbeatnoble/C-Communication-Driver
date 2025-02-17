26479089c6f675633c44117943289dd0aa7e199c
fannuel <fannuelgulelat74@gmail.com>
2025-02-17 18:21:42 +0300

    the client

---
+++ TCP Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+class TCPCLeint
+{
+    static void Main(string[] args)
+    {
+        string serverIp = "127.0.0.1";
+        int port = 5000;
+
+        try
+        {
+            using (TcpClient client = new TcpClient(serverIp, port))
+            {
+                Console.WriteLine("Connected to server.");
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    Console.Write("Enter command (GET_TEMP, GET_STATUS, EXIT): ");
+                    string command = Console.ReadLine();
+
+                    if (command.ToUpper() == "EXIT")
+                        break;
+
+                    byte[] data = Encoding.ASCII.GetBytes(command);
+                    stream.Write(data, 0, data.Length);
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                    Console.WriteLine($"Response: {response}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Client error: {ex.Message}");
+        }
+    }
+}
